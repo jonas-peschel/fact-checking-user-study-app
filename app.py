d6cd365acfd21bd021a20416361d63788c3d1529
jonas-peschel <jonaspeschel@gmx.de>
2025-09-25 15:13:37 +0200

    added dummy surveys

--- app.py
+++ app.py
@@ -12,4 +12,12 @@ pid = st.session_state.participant_id
 
 st.title("Fact-checking User Study")
 
-iframe(f"https://jonas-peschel.github.io/fact-checking-surveys/test_survey/?pid={pid}", height=1000)
+
+st.write("Pre-study Survey")
+iframe(f"https://jonas-peschel.github.io/fact-checking-surveys/pre/?pid={pid}&claim_id=pre", height=1000)
+
+st.write("Per-claim Survey")
+iframe(f"https://jonas-peschel.github.io/fact-checking-surveys/claim/?pid={pid}&claim_id={100}", height=1000)
+
+st.write("Post-study Survey")
+iframe(f"https://jonas-peschel.github.io/fact-checking-surveys/post/?pid={pid}&claim_id=post", height=1000)
